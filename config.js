27264ae7f1bdfbca21db41718df657c5d8316946
SOSLABBPN <soslabbpn@gmail.com>
2024-12-13 17:01:31 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+/*
+* Theasys
+* Generated on: Friday 13th of December 2024 05:34:10 AM
+* Config file: You can change the path to match your server path.
+*/
+
+var vars = {
+    file : 'index.html',
+    path : 'http://localhost/vt/',//path to the index.html
+    theme : 'theasys',
+    google_maps_api_key : 'YOUR_API_KEY_HERE',
+};
